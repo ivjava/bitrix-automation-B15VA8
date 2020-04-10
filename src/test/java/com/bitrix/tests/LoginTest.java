902d4ddc6e531879182307a112ebc2c129902fe6
Ilya <58124889+ivjava@users.noreply.github.com>
2020-04-10 19:40:37 -0400

    Fixed the Login in test and Created separate class for LoginTest

--- src/test/java/com/bitrix/tests/LoginTest.java
+++ src/test/java/com/bitrix/tests/LoginTest.java
@@ -1,24 +1,22 @@
 package com.bitrix.tests;
 
 import com.bitrix.base.TestBase;
-import com.bitrix.utilities.ConfigurationReader;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class BitrixPollPageTestsIV extends TestBase {
-
+public class LoginTest extends TestBase {
 
     @Test
     public void logInTest()
     {
         test = report.createTest("Login Method Test (hr_user)");
-        test.info("Testing Login method");
+        test.info("logging in as hr_user");
         loginPage.login("hr_user");
-        Assert.assertTrue(driver.getTitle().contains("Portal"));
+        test.info("Verifying Page Title");
+        Assert.assertTrue(driver.getTitle().contains("Portal"), "Expected: Portal");
         test.pass("Log in successful");
 
     }
+
 
 }
